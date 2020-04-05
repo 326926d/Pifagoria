e546a85abe7dd23c1088860a9f5cd74e976feeef
Dan_Nazan <danzan@list.ru>
2020-04-05 19:21:07 +0300

    clon

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace webdevelop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
